b32c3d460ee3c190a0d1348ea9885e7f61f2cc76
abeljosetoms115 <abeljosetoms115@hotmail.com>
2022-07-04 04:40:13 -0700

    FEAT: add configuration do entity

---
+++ AjmdsControloPresenca.Infra/Entity/Data/EntityDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjmdsControloPresenca.Infra.Entity.Data
+{
+    public class EntityDatabaseContext:DbContext
+    {
+        public EntityDatabaseContext():base("ConnectionStringSQL")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+    }
+}
