c47cfcc83516eeb0adb606d4400db93e957e51e5
abeljosetoms115 <abeljosetoms115@hotmail.com>
2022-07-04 06:08:36 -0700

    FEAT: add dbsets

--- AjmdsControloPresenca.Infra/Entity/Data/EntityDatabaseContext.cs
+++ AjmdsControloPresenca.Infra/Entity/Data/EntityDatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AjmdsControloPresenca.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -7,12 +8,19 @@ using System.Threading.Tasks;
 
 namespace AjmdsControloPresenca.Infra.Entity.Data
 {
-    public class EntityDatabaseContext:DbContext
+    public class EntityDatabaseContext : DbContext
     {
-        public EntityDatabaseContext():base("ConnectionStringSQL")
+        public EntityDatabaseContext() : base("ConnectionStringSQL")
         {
             Configuration.LazyLoadingEnabled = false;
             Configuration.ProxyCreationEnabled = false;
         }
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<DepartamentoTurno> DepartamentoTurnos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Presenca> Presencas { get; set; }
+        public DbSet<Turno> Turnos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
