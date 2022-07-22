baa70b7f941cf9d8b4c8601cfe8690c9d0aa1a53
abeljosetoms115 <abeljosetoms115@hotmail.com>
2022-07-22 07:07:49 -0700

    FEAT: add repository padrao

---
+++ AjmdsControloPresenca.Domain/Contract/IRepositoryCrud.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AjmdsControloPresenca.Domain.Contract
+{
+    public interface IRepositoryCrud<T> : IDisposable where T : class
+    {
+        T ListarPorId(object id);
+        IEnumerable<T> ListarTodos();
+
+        void Add(T entidade);
+        void Alter(T entidade);
+        void Delete(T entidade);
+    }
+}
---
+++ AjmdsControloPresenca.Infra/Repository/RepositoryEF.cs
@@ -0,0 +1,34 @@
+﻿using AjmdsControloPresenca.Domain.Contract;
+using AjmdsControloPresenca.Infra.Entity.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AjmdsControloPresenca.Infra.Repository
+{
+    public class RepositoryEF<T> : IRepositoryCrud<T> where T : class
+    {
+        EntityDatabaseContext Context = new EntityDatabaseContext();
+        public IEnumerable<T> ListarTodos() => Context.Set<T>().ToList();
+
+        public T ListarPorId(object id) => Context.Set<T>().Find(id);
+
+        public void Add(T entidade)
+        {
+            Context.Set<T>().Add(entidade);
+            SaveContext();
+        }
+        public void Delete(T entidade)
+        {
+            Context.Set<T>().Remove(entidade);
+            SaveContext();
+        }
+        public void Alter(T entidade)
+        {
+            Context.Entry(entidade).State = System.Data.Entity.EntityState.Modified;
+            SaveContext();
+        }
+        private void SaveContext() => Context.SaveChanges();
+
+        public void Dispose() => Context.Dispose();
+    }
+}
