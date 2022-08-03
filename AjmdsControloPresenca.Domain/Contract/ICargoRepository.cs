292e90d2b0c6dbc6e512d88e2300082fd8f76a4a
Abeltomas16 <abeljosetoms115@gmail.com>
2022-08-03 23:59:16 +0100

    FEAT: add ICargos, IEstadoCivil e IGenero

---
+++ AjmdsControloPresenca.Domain/Contract/ICargoRepository.cs
@@ -0,0 +1,10 @@
+﻿using AjmdsControloPresenca.Domain.Entities;
+using System.Collections.Generic;
+
+namespace AjmdsControloPresenca.Domain.Contract
+{
+    public interface ICargoRepository : IRepositoryCrud<Cargo>
+    {
+        IEnumerable<Cargo> ListarTodos();
+    }
+}
---
+++ AjmdsControloPresenca.Domain/Contract/IEstadoCivilRepository.cs
@@ -0,0 +1,11 @@
+﻿using AjmdsControloPresenca.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace AjmdsControloPresenca.Domain.Contract
+{
+    public interface IEstadoCivilRepository : IDisposable
+    {
+        IEnumerable<EstadoCivil> ListarTodos();
+    }
+}
