47e83c45253479615afddccac846ed024713c802
Abeltomas16 <abeljosetoms115@gmail.com>
2022-08-11 20:24:12 +0100

    FEAT: Add Validacao

--- AjmdsControloPresenca.UI/Models/Cargo/CargoIndexVM.cs
+++ AjmdsControloPresenca.UI/Models/Cargo/CargoIndexVM.cs
@@ -1,8 +1,12 @@
-﻿namespace AjmdsControloPresenca.UI.Models.Cargo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AjmdsControloPresenca.UI.Models.Cargo
 {
     public class CargoIndexVM
     {
         public short Id { get; set; }
+        [Required]
+        [Display(Name = "Nome"), StringLength(50)]
         public string Descricao { get; set; }
     }
 }
