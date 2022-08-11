67eba62f1deff38f1960784be1beca4547d9a6d4
Abeltomas16 <abeljosetoms115@gmail.com>
2022-08-11 21:15:43 +0100

    FEAT: Add ITurnoRepository and TurnoRepository

---
+++ AjmdsControloPresenca.Domain/Contract/ITurnoRepository.cs
@@ -0,0 +1,10 @@
+﻿using AjmdsControloPresenca.Domain.Entities;
+using System.Collections.Generic;
+
+namespace AjmdsControloPresenca.Domain.Contract
+{
+    public interface ITurnoRepository:IRepositoryCrud<Turno>
+    {
+        IEnumerable<Turno> ListarTodos();
+    }
+}
