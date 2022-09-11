6ea8d55558794ac36ab95b1818b29fadafad2613
Abeltomas16 <abeljosetoms115@gmail.com>
2022-09-11 13:48:01 +0100

    Add controller and view TurnoFunc

---
+++ AjmdsControloPresenca.UI/Controllers/TurnoFuncController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjmdsControloPresenca.UI.Controllers
+{
+    public class TurnoFuncController : Controller
+    {
+        // GET: TurnoFunc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
