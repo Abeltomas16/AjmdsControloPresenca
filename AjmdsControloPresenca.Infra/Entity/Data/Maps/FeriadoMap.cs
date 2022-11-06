09b3bd0c297f63d7f6aebc5583ec211113e17f21
Abel Tomás José <abeljosetoms115@gmail.com>
2022-11-06 23:01:07 +0100

    FEAT: add feriadoMap

---
+++ AjmdsControloPresenca.Infra/Entity/Data/Maps/FeriadoMap.cs
@@ -0,0 +1,26 @@
+﻿using AjmdsControloPresenca.Domain.Entities;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+
+namespace AjmdsControloPresenca.Infra.Entity.Data.Maps
+{
+    public class FeriadoMap : EntityTypeConfiguration<Feriado>
+    {
+        public FeriadoMap()
+        {
+            ToTable("Feriado");
+
+            HasKey(pk => pk.FeriadoId);
+
+            Property(c => c.FeriadoId)
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(c => c.Descricao)
+                .HasMaxLength(100)
+                .HasColumnAnnotation("IX_DESCRICAO", new IndexAnnotation(new System.ComponentModel.DataAnnotations.Schema.IndexAttribute()
+                {
+                    IsUnique = true
+                }));
+        }
+    }
+}
