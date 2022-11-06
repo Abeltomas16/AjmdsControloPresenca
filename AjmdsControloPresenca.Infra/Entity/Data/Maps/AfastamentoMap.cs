8a4b6e7ea87d2b514ff87643a076542650165543
Abel Tomás José <abeljosetoms115@gmail.com>
2022-11-06 23:08:56 +0100

    FEAT: add AfastamentoMap

---
+++ AjmdsControloPresenca.Infra/Entity/Data/Maps/AfastamentoMap.cs
@@ -0,0 +1,38 @@
+﻿using AjmdsControloPresenca.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace AjmdsControloPresenca.Infra.Entity.Data.Maps
+{
+    public class AfastamentoMap : EntityTypeConfiguration<Afastamento>
+    {
+        public AfastamentoMap()
+        {
+            ToTable("Afastamento");
+
+            HasKey(pk => pk.AfastamentoId);
+
+            Property(c => c.AfastamentoId)
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(c => c.Observacao)
+             .HasMaxLength(100)
+            .HasColumnType("varchar");
+
+            Property(c => c.Estado);
+
+            Property(c => c.Apurado);
+
+            Property(c => c.SituacaoId);
+
+            Property(c => c.FuncionarioId);
+
+            HasRequired(fk => fk.Situacao)
+                 .WithMany(g => g.Afastamentos)
+                .HasForeignKey(x => x.SituacaoId);
+
+            HasRequired(fk => fk.Funcionario)
+                 .WithMany(c => c.Afastamentos)
+                 .HasForeignKey(x => x.FuncionarioId);
+        }
+    }
+}
