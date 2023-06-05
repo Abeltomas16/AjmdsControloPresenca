3f360ae965349d3df2acacf1785650958e4cd3a5
Abel Tomás José <abeljosetoms115@gmail.com>
2023-06-05 20:23:59 +0100

    FEAT: add novo template

--- AjmdsControloPresenca.UI/Controllers/LoginController.cs
+++ AjmdsControloPresenca.UI/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using AjmdsControloPresenca.UI.Models;
 using System;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Web.UI.WebControls;
 
 namespace AjmdsControloPresenca.UI.Controllers
 {
@@ -34,7 +35,7 @@ namespace AjmdsControloPresenca.UI.Controllers
 
                 if (ModelState.IsValid)
                 {
-                    FormsAuthentication.SetAuthCookie(login.Nome, login.Logado);
+                    FormsAuthentication.SetAuthCookie(login.Nome, true);
                     if (!string.IsNullOrEmpty(login.GoUrl) &&
                        Url.IsLocalUrl(login.GoUrl))
                         return Redirect(login.GoUrl);
@@ -48,6 +49,11 @@ namespace AjmdsControloPresenca.UI.Controllers
                 ModelState.AddModelError("ErroLogin", exc.Message);
                 return View(login);
             }
+        }
+        public ActionResult Logout(string ReturnUrl)
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("login", "login");
         }
     }
 }
--- AjmdsControloPresenca.UI/Models/LoginViewModel.cs
+++ AjmdsControloPresenca.UI/Models/LoginViewModel.cs
@@ -11,7 +11,7 @@ namespace AjmdsControloPresenca.UI.Models
         [Required(ErrorMessage = "O {0} é obrigatório")]
         [StringLength(30, ErrorMessage = "O limite do {0} é de {1} caracteres")]
         public string Senha { get; set; }
-        public bool Logado { get; set; }
+
         public string GoUrl { get; set; }
     }
 }
